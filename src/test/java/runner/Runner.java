0fe5029281648bc761d6dd4178d154f60724ca8b
Phan Văn Chinh <phanchinhnd1@gmail.com>
2024-03-21 23:37:13 +0700

    add code

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features = "src/test/resources/features/",
+        plugin = {"pretty", "json:target/cucumber.json"},
+        glue = {"steps"})
+public class Runner {
+}
